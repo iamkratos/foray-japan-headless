99afdc2820d130670f16044959c0eee3ffa08dad
Sam Davidoff <samjdavidoff@gmail.com>
2020-02-24 15:24:47 -0500

    meta des fixes

--- src/templates/pages/friends-of-foray-page.js
+++ src/templates/pages/friends-of-foray-page.js
@@ -126,10 +126,13 @@ const FriendsOfForay = ({ page, fallbackImg }) => {
     <Layout>
       <SEO
         title={page.title}
-        description={page.bodySummary
-          .split(" ")
-          .slice(0, 160)
-          .join(" ")}
+        description={
+          page.bodySummary &&
+          page.bodySummary
+            .split(" ")
+            .slice(0, 160)
+            .join(" ")
+        }
       >
         <meta name="og:image" content={window.location.host + fallbackImg} />
         <meta name="image" content={window.location.host + fallbackImg} />
