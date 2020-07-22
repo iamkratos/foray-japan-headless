ed8bdaba9bc88a5f0e10c965c7078e8aa4fb94ce
Sam Davidoff <samdavidoff@Sams-MBP.home>
2020-07-22 16:07:39 -0400

    new color split

--- src/templates/product.js
+++ src/templates/product.js
@@ -654,7 +654,14 @@ const ProductPage = ({ data }) => {
       );
     } else {
       handleVariantChange(product.images[0].altText);
-      setHoverColor(product.images[0].altText);
+      // setHoverColor(product.images[0].altText);
+      setHoverColor(
+        product.images[0].altText.toLowerCase() === "bw"
+          ? "B&W"
+          : product.images[0].altText.toLowerCase() === "nb"
+          ? "N&B"
+          : product.images[0].altText
+      );
     }
 
     console.log(sizeId);
