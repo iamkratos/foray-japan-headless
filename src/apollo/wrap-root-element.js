26e746b15cc6134668db710469945134f8479bdb
Sam Davidoff <samdavidoff@Sams-MBP.home>
2020-10-16 12:36:49 -0400

    bug fix 3

--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { StoreProvider } from "./src/context/StoreContext";
+import { StoreProvider } from "./../context/StoreContext";
+import fetch from "isomorphic-fetch";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://foray-golf-dev.myshopify.com/api/2019-07/graphql.json",
   cache: new InMemoryCache(),
+  fetch,
   headers: {
     "X-Shopify-Storefront-Access-Token": "5b871ba787e3d8597fcba89d138305fa",
     "Content-Type": "application/graphql",
@@ -14,7 +16,7 @@ const client = new ApolloClient({
 });
 
 export const wrapRootElement = ({ element }) => (
-  <StoreProvider>
-    <ApolloProvider client={client}>{element}</ApolloProvider>
-  </StoreProvider>
+  <ApolloProvider client={client}>
+    <StoreProvider>{element}</StoreProvider>
+  </ApolloProvider>
 );
